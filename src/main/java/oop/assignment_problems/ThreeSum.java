f54f1157dee4965b0ea7250e5aafa491e3e9c3fd
NIRAJ0905 <nirajpingale005@gmail.com>
2026-09-04 22:20:58 +0530

    feat: complete assignment and class problems for feature branch 2

---
+++ src/main/java/oop/assignment_problems/ThreeSum.java
@@ -0,0 +1,78 @@
+package oop.assignment_problems;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreeSum {
+
+    public static int[][] threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+
+        List<int[]> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    result.add(new int[]{
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    });
+
+                    left++;
+                    right--;
+
+                    while (left < right &&
+                           nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right &&
+                           nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        int[][] answer = new int[result.size()][3];
+
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        int[][] result = threeSum(nums);
+
+        System.out.println("Triplets:");
+
+        for (int[] triplet : result) {
+            System.out.println(Arrays.toString(triplet));
+        }
+    }
+}
